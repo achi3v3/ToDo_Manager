b90dc8fa3a44e45c8f547dd6ae7df7df7be4cd41
achi3v3 <amir-tutaev@mail.ru>
2025-05-13 17:32:23 +0500

    feat(back): add new handlers for task

--- back/database/task_handlers.go
+++ back/database/task_handlers.go
@@ -5,3 +5,23 @@ func Create_Task(task *Task) error {
 	result := db.Create(task)
 	return result.Error
 }
+
+func Get_All_Tasks() ([]Task, error) {
+	db := GetDB()
+	var tasks []Task
+	result := db.Find(&tasks)
+	return tasks, result.Error
+}
+
+func Get_Tasks_By_ID(UserID string) ([]Task, error) {
+	db := GetDB()
+	var tasks []Task
+	result := db.Where("user_id = ?", UserID).Find(&tasks)
+	return tasks, result.Error
+}
+
+func Delete_Task(task *Task) error {
+	db := GetDB()
+	result := db.Delete(task)
+	return result.Error
+}
